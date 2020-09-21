f31617caea19a63e04a82f523daa85c419cfbaf7
dusanpopov <aristocut90@gmail.com>
2020-09-21 15:58:29 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const hamburgerContainer = document.querySelector(".main-navbar");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const links = document.querySelectorAll(".main-navbar__item");
+
+
+hamburgerMenu.addEventListener("click", () => {
+    hamburgerContainer.classList.toggle("clicked");
+    links.forEach((link) => {
+        link.classList.toggle("fade");
+    })
+});
