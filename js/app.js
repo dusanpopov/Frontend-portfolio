daa02070d2040e75817fde6348d8de7ec6222e4d
Dušan Popov <aristocut90@gmail.com>
2021-01-07 19:03:04 +0100

    formatting

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const hamburgerContainer = document.querySelector(".main-navbar");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const links = document.querySelectorAll(".main-navbar__item");
 
-
 hamburgerMenu.addEventListener("click", () => {
     hamburgerContainer.classList.toggle("clicked");
     links.forEach((link) => {
